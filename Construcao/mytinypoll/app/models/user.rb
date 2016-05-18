68adfe468d42ca6f89ff128ddf15ff4c98d33d6c
vercoza <vercoza.thays@gmail.com>
2016-05-18 01:38:56 +0000

    Add new validations for users

--- Construcao/mytinypoll/app/models/user.rb
+++ Construcao/mytinypoll/app/models/user.rb
@@ -3,4 +3,7 @@ class User < ActiveRecord::Base
     validates_presence_of :email
     validates_presence_of :cpf
     validates_presence_of :password
+    
+    validates_uniqueness_of :email, message: "Email já cadastrado"
+    validates_uniqueness_of :cpf, message: "Cpf já cadastrado"
 end
