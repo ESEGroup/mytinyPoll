24affbb22d1d1bed54263afb671997fc258120b3
Heitor Guimarães <hguimaraes@poli.ufrj.br>
2016-05-21 19:10:19 +0000

    Recaptcha working

--- Construcao/mytinypoll/app/controllers/users_controller.rb
+++ Construcao/mytinypoll/app/controllers/users_controller.rb
@@ -30,15 +30,14 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.new(user_params)
-
     respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
-      else
-        format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+        if (verify_recaptcha and @user.save)
+          format.html { redirect_to @user, notice: 'User was successfully created.' }
+          format.json { render :show, status: :created, location: @user }
+        else
+          format.html { render :new }
+          format.json { render json: @user.errors, status: :unprocessable_entity }
+        end
     end
   end
 
@@ -46,7 +45,7 @@ class UsersController < ApplicationController
   # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
-      if @user.update(user_params)
+      if verify_recaptcha and @user.update(user_params)
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
@@ -67,15 +66,14 @@ class UsersController < ApplicationController
   end
   
   def authenticate (params_email, params_password)
-        test = false
-        user = User.find_by(email: params_email)
-        
-        if(user and user.password.eql? params_password)
-          test = true  
-        end
-        
-        return test
+    test = false
+    
+    user = User.find_by(email: params_email)
+    if(user and user.password.eql? params_password)
+      test = true  
     end
+    return test
+  end
   
   private
     # Use callbacks to share common setup or constraints between actions.
