8fd83cdb70e447a469e273fbb3054594608fd3cb
vercoza <vercoza.thays@gmail.com>
2016-05-25 21:53:59 +0000

    Remove cpf method -not used - of the users_helper

--- Construcao/mytinypoll/app/helpers/users_helper.rb
+++ Construcao/mytinypoll/app/helpers/users_helper.rb
@@ -1,30 +1,5 @@
 module UsersHelper
     
-    def check_cpf(cpf=nil)
-        return false if cpf.nil?
-        winvalid= %w{12345678909 11111111111 22222222222 33333333333 44444444444 55555555555 66666666666 77777777777 88888888888 99999999999 00000000000}
-        wcpf = cpf.scan #/[0-9]/
-        if cpf.length == 11
-            unless winvalid.member?(wcpf.join)
-                wcpf = wcpf.collect{|x| x.to_i}
-                wsum = 10*wcpf[0]+9*wcpf[1]+8*wcpf[2]+7*wcpf[3]+6*wcpf[4]+5*wcpf[5]+4*wcpf[6]+3*wcpf[7]+2*wcpf[8]
-                wsum = wsum - (11 * (wsum/11))
-                wresult = (wsum == 0 or wsum == 1) ? 0 : 11 - wsum
-                if wresult == wcpf[9]
-                    wsum = wcpf[0]*11+wcpf[1]*10+wcpf[2]*9+wcpf[3]*8+wcpf[4]*7+wcpf[5]*6+wcpf[6]*5+wcpf[7]*4+wcpf[8]*3+wcpf[9]*2
-                    wsum = wsum - (11 * (wsum/11))
-                    wnewresult = (wsum == 0 or wsum == 1) ? 0 : 11 - wsum
-                    if wnewresult == wcpf[10] # CPF validado
-                        return true 
-                    else
-                        return false # CPF invalidado 
-                    end
-                end
-            end
-        end
-        return false # CPF invalidado
-    end
-    
     def authenticate_mail(email)
         return email =~ /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
     end
--- Construcao/mytinypoll/app/models/user.rb
+++ Construcao/mytinypoll/app/models/user.rb
@@ -12,11 +12,6 @@ class User < ActiveRecord::Base
     validates_format_of :email, with:/\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i, message: "Email inválido"
     validates_length_of :cpf, :is => 11, message: "O cpf deve possuir 11 dígitos"
     
-    validates :cpf, cpf: true # <-- custom cpf validator
-    
-    #validates_each :cpf, allow_blank: false do |record, attr, value|
-    #    record.errors.add attr, 'Cpf inválido' if !check_cpf(value) 
-    #    end
-    #end
+    validates :cpf, cpf: true # <-- chama cpf_validator
     
 end
