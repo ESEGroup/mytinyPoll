cafc5c6e97303cbee93fa82c44805dcd6d445ac6
Heitor Guimarães <hguimaraes@poli.ufrj.br>
2016-06-06 00:59:00 +0000

    Fixing login recaptcha

--- Construcao/mytinypoll/app/controllers/sessions_controller.rb
+++ Construcao/mytinypoll/app/controllers/sessions_controller.rb
@@ -1,14 +1,54 @@
+##
+#  This class/controller is responsible for the user login and logout in the
+# system.
+
 class SessionsController < ApplicationController
+  before_filter :set_missedlogin
+  
+  ##
+  #  Initializes useful variables to check for authentication erros stored in
+  # the session.
+  # More explanation (and reference) at: http://goo.gl/34G1b4 
+  
+  def set_missedlogin
+    session[:missedlogin] ||= false
+  end
+  
+  ##
+  #  Method to Log In the user in the system.
+  # Reset the sessions after executes the main puporse or display a recaptcha if
+  # the user miss the login.
+  
   def login
     usercontroller = UsersController.new
     loggedin = usercontroller.authenticate(params[:session][:email], params[:session][:password])
-    if (loggedin)
-      render text: "Usuário Logado com sucesso!"
+    
+    # Case the user has missed the first login attempt, he need to pass in the
+    # recaptcha to prove he isn't a bot.
+    if session[:missedlogin]
+      if (verify_recaptcha and loggedin)
+        render text: "Usuário Logado com sucesso!"
+      else
+        session[:missedlogin] = true
+        redirect_to root_path, :error => "Login, senha ou ReCaptcha inválido!"
+      end
+    
+    # First attempt to login in the system will be executed here.
     else
-      redirect_to root_path, :error => "Login ou senha inválido"
+      if (loggedin)
+        render text: "Usuário Logado com sucesso!"
+        reset_session
+      else
+        session[:missedlogin] = true
+        redirect_to root_path, :error => "Login ou senha inválido!"
+      end
     end
   end
 
+  ##
+  #  Method to logout the user from the system and restore all 
+  # session variables.
+  
   def logout
   end
 end
