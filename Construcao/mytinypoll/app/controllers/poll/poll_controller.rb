feb07747b64c0d1e026a10d55cab6b118e0e4fd9
Heitor Guimarães <hguimaraes@poli.ufrj.br>
2016-07-20 00:40:56 +0000

    Work around to deal with the options in the view/controller. Need to refactor to use dynamic-forms, but not now.

--- Construcao/mytinypoll/app/controllers/poll/poll_controller.rb
+++ Construcao/mytinypoll/app/controllers/poll/poll_controller.rb
@@ -1,15 +1,21 @@
 class Poll::PollController < ApplicationController
     def new_poll
         if params[:discpoll]
-            ::Poll::Poll.create!(poll_params)
+            ::Poll::Poll.create!(discursive_poll_params)
         else
-            ::Poll::Objective::ObjectivePoll.create!(poll_params)
+            ::Poll::Objective::ObjectivePoll.create!(objective_poll_params)
         end
         redirect_to root_path
     end
     
     private
-    def poll_params
-        params.require(:poll).permit(:title, :privacy, :poll_avatar, :question, :deadline, :user_id)
-    end
+        def discursive_poll_params
+            params.require(:poll).permit(:title, :privacy, :poll_avatar,
+                :question, :deadline, :user_id)
+        end
+    
+        def objective_poll_params
+            params.require(:poll).permit(:title, :privacy, :poll_avatar,
+                :question, :deadline, :user_id)
+        end
 end
