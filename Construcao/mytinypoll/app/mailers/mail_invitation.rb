5e66b40e79f88bfac62cf39cf01b38ff99781ffd
beremizsinistro <beremizsinistro@gmail.com>
2016-07-25 00:10:48 +0000

    tentando email2

--- Construcao/mytinypoll/app/mailers/mail_invitation.rb
+++ Construcao/mytinypoll/app/mailers/mail_invitation.rb
@@ -1,11 +1,11 @@
 class MailInvitation < ApplicationMailer
     default from: 'mytinypoll@gmail.com'
     default to: 'beremizsinistro@gmail.com'
-    def send_invitation(lista.email)
+    def creator_permission_email()
         Rails.logger.debug("debug:: dentro do email")
-        mail(subject: 'We are inviting you to our poll')
+        #mail(subject: 'We are inviting you to our poll')
         @url = 'http//www.gmail.com'
-        mail(to: lista.email, subject: 'We are inviting you to our poll')
+        mail(to: 'beremisinistro@gmail.com', subject: 'We are inviting you to our poll')
         Rails.logger.debug("debug:: depois do email")
     end
 end
